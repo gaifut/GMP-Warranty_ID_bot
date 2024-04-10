8d7f867a0639e7f783f718c87b8cf9323ddd8bd4
Artur Gayfutdinov <gaifut@gmail.com>
2024-04-10 18:16:39 +0300

    Update GMP_W_B_main 1.3.py
    move secrets to .env, apply PEP8 practices in code design

--- GMP_W_B_main 1.3.py
+++ GMP_W_B_main 1.3.py
@@ -1,45 +1,54 @@
+import logging
+import os
+import time
+
+import gspread
 import pandas as pd
 from aiogram import Bot, Dispatcher, types, executor
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import time
-import logging
-import gspread
+from aiogram.dispatcher import FSMContext
+from aiogram.types import InlineKeyboardMarkup
+from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
 
 import sensitive as s
 import definitions as d
 
-# connect to Google Sheets
+load_dotenv()
 
-sa = gspread.service_account(filename = s.Google_sheets_API_details)
+API_KEY = os.getenv('API_KEY')
+bot_username = os.getenv('bot_username')
+bot_name = os.getenv('bot_name')
+Google_sheets_API_details = os.getenv('Google_sheets_API_details')
+FEEDS = os.getenv('FEEDS')
+DRIVE = os.getenv('DRIVE')
+
+
+# connect to Google Sheets
+sa = gspread.service_account(filename=Google_sheets_API_details)
 sheet_connect = sa.open(d.workbook_name)
 # wks stands for worksheet
 wks = sheet_connect.worksheet(d.worksheet_name)
-
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name(s.Google_sheets_API_details, scope)
+scope = [FEEDS, DRIVE]
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    Google_sheets_API_details, scope)
 gc = gspread.authorize(credentials)
 
 # Configure logging
-logging.basicConfig(filename = d.logs_filename,level=logging.INFO)
+logging.basicConfig(filename=d.logs_filename, level=logging.INFO)
 
 # timeout
 bot_timeout = d.session_timeout
 
 # Accessing warranty table
-data_W = pd.read_excel(s.excel_file_location_W, sheet_name = 'Гарантийные')
+data_W = pd.read_excel(s.excel_file_location_W, sheet_name='Гарантийные')
 df_W = pd.DataFrame(data_W)
-df_W.set_index(['Наш ID'], inplace = True)
+df_W.set_index(['Наш ID'], inplace=True)
 
 # Accessing spare parts table
-data_S = pd.read_excel(s.excel_file_location_S, sheet_name = 'Поставки на нас')
+data_S = pd.read_excel(s.excel_file_location_S, sheet_name='Поставки на нас')
 df_S = pd.DataFrame(data_S)
-df_S.set_index(['ID'], inplace = True)
+df_S.set_index(['ID'], inplace=True)
 
 
 # Human-bot interaction
@@ -51,18 +60,18 @@ async def set_bot_commands():
     await bot.set_my_commands(commands)
 
 # Bot info
-bot = Bot (s.API_KEY)
+bot = Bot(API_KEY)
 storage = MemoryStorage()
-dp = Dispatcher(bot, storage = storage)
-
+dp = Dispatcher(bot, storage=storage)
 
 # keyboard
 keyboard_inline = InlineKeyboardMarkup().add(d.button1).add(d.button2)
 
 
 @dp.message_handler(commands=['help'], state='*')
-async def help_message (message: types.Message):
-    await message.answer (d.help_intro)
+async def help_message(message: types.Message):
+    await message.answer(d.help_intro)
+
 
 @dp.message_handler(commands=['start'], state='*')
 async def choose_inquiry(message: types.Message, state: FSMContext):
@@ -70,10 +79,11 @@ async def choose_inquiry(message: types.Message, state: FSMContext):
     await state.reset_state()
     await state.update_data(session_start_time=time.time())
     await state.update_data(query_count=0)
-    await state.update_data(user_no = user_id)
+    await state.update_data(user_no=user_id)
     user_name = message.from_user.first_name
     logging.info(f'{user_id} {user_name} {time.asctime()}')
-    await message.answer (text = f"{user_name}, {d.introduction}", reply_markup=keyboard_inline)
+    await message.answer(
+        text=f"{user_name}, {d.introduction}", reply_markup=keyboard_inline)
 
     data_start_handler_time = await state.get_data()
     session_start_time = data_start_handler_time.get("session_start_time")
@@ -83,46 +93,56 @@ async def choose_inquiry(message: types.Message, state: FSMContext):
         return
 
 
-@dp.callback_query_handler(lambda call: call.data in ['warranty', 'spareparts_sales'])
+@dp.callback_query_handler(
+        lambda call: call.data in ['warranty', 'spareparts_sales'])
 async def inline_buttons_handler(call: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query_id=call.id)
     if call.data == 'warranty':
         await state.update_data(warranty_chosen=call.data)
-        await call.message.answer (d.enter_warranty_ID)
-        await d.warranty_state.provide_ID.set()
+        await call.message.answer(d.enter_warranty_ID)
+        await d.Warranty_state.provide_ID.set()
     elif call.data == 'spareparts_sales':
-        await state.update_data(spareparts_sales_chosen = call.data)
+        await state.update_data(spareparts_sales_chosen=call.data)
         await call.message.answer(d.enter_spareparts_sales_ID)
-        await d.spareparts_sales_state.provide_ID.set()
+        await d.Spareparts_sales_state.provide_ID.set()
 
-    
-@dp.message_handler(state=d.warranty_state.provide_ID)
-async def receive_ID_info (message: types.Message, state: FSMContext):
-    try:   
-        get_current_value_for_query_count =  await state.get_data() # get the current state for query count
-        query_count = get_current_value_for_query_count.get("query_count", 0) #get the value of the query count from the current state or 0 if it doesnt exist
+
+@dp.message_handler(state=d.Warranty_state.provide_ID)
+async def receive_ID_info(message: types.Message, state: FSMContext):
+    try:
+        # get the current state for query count
+        get_current_value_for_query_count = await state.get_data()
+        # get the value of the query count from the current state
+        # or 0 if it doesnt exist
+        query_count = get_current_value_for_query_count.get("query_count", 0)
         if query_count >= d.query_limit:
-            await message.answer (d.querry_limit_exceed_message)
+            await message.answer(d.querry_limit_exceed_message)
             await state.finish()
             return
-        
-        await state.update_data(query_count= query_count + 1)
-        await state.update_data(provide_ID = message.text)
+
+        await state.update_data(query_count=query_count + 1)
+        await state.update_data(provide_ID=message.text)
         warranty_state_dictionary = await state.get_data()
         ID_info_W = (f"{warranty_state_dictionary['provide_ID']}")
         status_W = df_W.loc[f'{ID_info_W}', 'Статус полный для табл']
         status_W_ID_text = (f'По ID {ID_info_W} статус: {status_W}')
-        await message.answer (status_W_ID_text)
-        await d.warranty_state.provide_ID.set()
+        await message.answer(status_W_ID_text)
+        await d.Warranty_state.provide_ID.set()
     except KeyError:
         await message.answer(d.error_message)
     except Exception as e:
         await message.answer(f"An error occurred: {e}")
     finally:
         data_receive_ID_info_W_handler_time = await state.get_data()
-        session_start_time = data_receive_ID_info_W_handler_time.get("session_start_time")
-        print (warranty_state_dictionary)
-        wks.append_row([warranty_state_dictionary['user_no'], warranty_state_dictionary['provide_ID'], warranty_state_dictionary['session_start_time'],warranty_state_dictionary['query_count']])
+        session_start_time = data_receive_ID_info_W_handler_time.get(
+            "session_start_time")
+        print(warranty_state_dictionary)
+        wks.append_row(
+            [warranty_state_dictionary['user_no'],
+             warranty_state_dictionary['provide_ID'],
+             warranty_state_dictionary['session_start_time'],
+             warranty_state_dictionary['query_count']]
+        )
         if time.time() - session_start_time > d.session_timeout:
             await message.answer(d.session_timeout_message)
             await state.finish()
@@ -132,38 +152,47 @@ async def receive_ID_info (message: types.Message, state: FSMContext):
             await state.finish()
             await message.answer("Отменено")
 
-@dp.message_handler(state=d.spareparts_sales_state.provide_ID)
+
+@dp.message_handler(state=d.Spareparts_sales_state.provide_ID)
 async def receive_sales_ID_info(message: types.Message, state: FSMContext):
     try:
-        get_current_value_for_query_count =  await state.get_data() # get the current state for query count
-        query_count = get_current_value_for_query_count.get("query_count", 0) #get the value of the query count from the current state or 0 if it doesnt exist
+        # get the current state for query count
+        get_current_value_for_query_count = await state.get_data()
+        # get the value of the query count from the current state
+        # or 0 if it doesnt exist
+        query_count = get_current_value_for_query_count.get("query_count", 0)
         if query_count >= d.query_limit:
-            await message.answer (d.querry_limit_exceed_message)
+            await message.answer(d.querry_limit_exceed_message)
             await state.finish()
             return
-        
-        await state.update_data(query_count= query_count + 1)
-        await state.update_data(provide_ID = message.text)
+
+        await state.update_data(query_count=query_count + 1)
+        await state.update_data(provide_ID=message.text)
         spareparts_sales_dictionary = await state.get_data()
         ID_info_S = (f"{spareparts_sales_dictionary['provide_ID']}")
         status_S = df_S.loc[f'{ID_info_S}', 'Статус внешний']
         status_S_ID_text = (f'По ID {ID_info_S} статус: {status_S}')
-        await message.answer (status_S_ID_text)
-        await d.spareparts_sales_state.provide_ID.set()
-        print (spareparts_sales_dictionary)
-        wks.append_row([spareparts_sales_dictionary['user_no'], spareparts_sales_dictionary['provide_ID'], spareparts_sales_dictionary['session_start_time'],spareparts_sales_dictionary['query_count']])
+        await message.answer(status_S_ID_text)
+        await d.Spareparts_sales_state.provide_ID.set()
+        print(spareparts_sales_dictionary)
+        wks.append_row(
+            [spareparts_sales_dictionary['user_no'],
+             spareparts_sales_dictionary['provide_ID'],
+             spareparts_sales_dictionary['session_start_time'],
+             spareparts_sales_dictionary['query_count']]
+        )
     except KeyError:
         await message.answer(d.error_message)
     except Exception as e:
         await message.answer(f"An error occurred: {e}")
     finally:
         data_receive_ID_info_W_handler_time = await state.get_data()
-        session_start_time = data_receive_ID_info_W_handler_time.get("session_start_time")
+        session_start_time = data_receive_ID_info_W_handler_time.get(
+            "session_start_time")
         if time.time() - session_start_time > d.session_timeout:
             await message.answer(d.session_timeout_message)
             await state.finish()
-            return  
-
+            return
 
 if __name__ == '__main__':
     executor.start_polling(dp)
